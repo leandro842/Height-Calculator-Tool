71a003a90a893b3cd8f69267b0ff983fbe5db33f
Leandro Rodriguês <l842law@hotmail.com>
2020-08-17 19:41:40 -0300

    Add files via upload

---
+++ Height Calculator Tool/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Height_Calculator_Tool
+{
+    public partial class Form1 : Form
+    { 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static int txtBox_value = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtBox_value = Convert.ToInt16(this.textBox1.Text);
+                Form2 form2 = new Form2(this);
+                form2.Show();
+                this.Enabled = false;
+            }
+            catch
+            {
+                MessageBox.Show("Input a valid height!");
+            }
+
+        }
+    }
+}
---
+++ Height Calculator Tool/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Height_Calculator_Tool
+{
+    public partial class Form2 : Form
+    {
+        private Form1 _frm1 = null;
+        public Form2(Form frm)
+        {
+            _frm1 = frm as Form1;
+            InitializeComponent();
+            this.label1.Text = ("Your height is " + Form1.txtBox_value.ToString() + " cm!");
+        }
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _frm1.Enabled = true;
+        }
+    }
+}
